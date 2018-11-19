4e75f4a79ffbd0aef69a0a70edafc7b25a7d56f0
Toshiki <toshikijahja@gmail.com>
2018-11-19 00:44:04 -0800

    Fix margin on poll

--- api/constants.js
+++ api/constants.js
@@ -1,4 +1,4 @@
-export const URL = "http://dce730e0.ngrok.io/buzz";
+export const URL = "http://907f3e92.ngrok.io/buzz";
 export const tokenKey = "token-key";
 // ./ngrok http 8080
 // export const URL = "http://localhost:8080/buzz";
